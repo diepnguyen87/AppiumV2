8f3b26874acb0564f313f6bad127c0658c10fbdb
Diep Nguyen <diep.nguyen@itech.asia>
2026-07-27 09:59:32 +0700

    remove clone source code on reusable-deploy.yml

--- src/main/java/api_learning/AppPackages.java
+++ src/main/java/api_learning/AppPackages.java
@@ -4,5 +4,5 @@ public interface AppPackages {
 
     String SETTING = "com.android.settings";
     String WDIO = "com.wdiodemoapp";
-    String BUNDLEID = "org.wdiodemoapp";
+    String BUNDLEID_WDIO = "org.wdiodemoapp";
 }
--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -61,7 +61,7 @@ public class DriverFactory implements MobileCapabilityType, AppPackages {
                 case IOS -> {
                     caps.setCapability(AUTOMATION_NAME, "xcuitest");
                     caps.setCapability(UDID, udid);
-                    caps.setCapability(BUNDLE_ID, AppPackages.BUNDLEID);
+                    caps.setCapability(BUNDLE_ID, AppPackages.BUNDLEID_WDIO);
                     caps.setCapability(WDA_LOCAL_PORT, systemPort);
                     driver = new IOSDriver(appiumServer, caps);
                 }
